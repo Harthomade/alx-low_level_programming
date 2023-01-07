8f1e78148dfffdddfe1c9dbf58423b4fdcbf73d2
Harthomade <adeyinkatemitope53@gmail.com>
2023-01-06 19:38:28 -0800

    array that contains the program command line arguments.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <ctype.h>
 /**
- * main - adds possitive numbers.
+ * main - adds positive numbers.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int i, j. add =0;
+	int i, j, add = 0;
 
-	for (i = 1; i< argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
