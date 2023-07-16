0a8d27805d0479119244af239d9e85a02179dcda
Harthomade <adeyinkatemitope53@gmail.com>
2023-07-15 17:32:48 -0700

    the structure of debugging

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,27 +1,26 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers
- * using the Linear search algorithm
- * @array: a pointer to the first element of the array to search in
- * @size: is the number of elements in array
- * @value: is the value to search for
- * Return: the first index where value is located or
- * return 1 If value is not present in array or if array is NULL
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: Always EXIT_SUCCESS
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	int i;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
-	while (i < size)
+
+	for (i = 0; i < (int)size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return ((int)i);
-		i++;
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
 	}
 	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,48 +1,61 @@
 #include "search_algos.h"
 
 /**
- * print_sub - prints the sub array
- * @array: the subarray
- * @high: highest index
- * @low: lowest index
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
-void print_sub(int *array, size_t high, size_t low)
+int recursive_search(int *array, size_t size, int value)
 {
+	size_t half = size / 2;
 	size_t i;
 
-	printf("Searching in array: ");
-	for (i = low; i < high; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[high]);
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
 }
 
 /**
- * binary_search - searches for a value in a sorted array
- * of integers using the Binary search algorithm
- * @array: is a pointer to the first element of the array to search in
- * @size: is the number of elements in array
- * @value: is the value to search for
- * Return: the index where value is located or
- * -1 If value is not present in array or if array is NULL
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
-
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low = 0, high, mid;
+	int index;
 
-	if (!array)
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
 		return (-1);
-	high = size - 1;
-	while (low <= high)
-	{
-		print_sub(array, high, low);
-		mid = low + (high - low) / 2;
-		if (array[mid] < value)
-			low = mid + 1;
-		else if (array[mid] > value)
-			high = mid - 1;
-		else
-			return (mid);
-	}
-	return (-1);
+
+	return (index);
 }
